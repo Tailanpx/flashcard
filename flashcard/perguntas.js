b1fadcc06af56cbf928786872a1efe8f73452523
Tailanpx <sartor.tailan@escola.pr.gov.br>
2025-07-04 16:53:32 -0300

    Add files via upload

---
+++ flashcard/perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Programação',
+    'Qual e a capital da Alemanha?',
+    'Berlim'
+)
+
+criaCartao(
+    'Geografia',
+    'Em que continente fica o Brasil?',
+    'América do Sul'
+)
+
+criaCartao(
+    'Programação',
+    'Quantos dias tem 10 anos(355 dias)?',
+    '3650 dias'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é o maior animal do mundo?',
+    'Baleia-azul'
+)
+
+criaCartao(
+    'Programação',
+    'Quem é o ataul presidente do EUA?',
+    'Donald Trump'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é o seguando maior país da América do sul?',
+    'Argentina'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é o maior paś da afríca?',
+    'Argélia'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é o maior país do mundo?',
+    'Russia'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é o menor país da América do sul?',
+    'Suriname'
+)
